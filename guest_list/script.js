5858955ea22ea1aa992f262857c9b63c70bf49a4
Ken <kennethabreu1@gmail.com>
2021-11-18 08:47:51 -0500

    addPerson function creates p tag

--- guest_list/script.js
+++ guest_list/script.js
@@ -1,15 +1,29 @@
+function addPerson(array) {
+    //create new p element
+    const newPara = document.createElement("p");
+
+    //give p element some content
+    const fillGuest = document.createTextNode(array);
+
+    //add text node to newly created p element
+    newPara.appendChild(fillGuest);
+
+    //add newly created p element and its contents to DOM
+    const body = document.getElementById(null); //setting getElementById to null or leaving empty just adds right to body of html
+    document.body.insertBefore(newPara, body);
+}
+
 let people = ["Gob", "Buster", "Lucille", "Michael", "Lindsay", "Tobias", "George-Michael", "Maeby"];
 let guestList = [];
 let refusedEntry = [];
 
 for (i = 0; i < people.length; i++) {
     if (people[i] === "George-Michael" || people[i] === "Maeby") {
-        console.log(`Sorry ${people[i]}, no kids allowed.`);
+        addPerson(`Sorry ${people[i]}, no kids allowed.`);
         refusedEntry.push(people[i]);
     }
     else {
-        console.log(`Welcome to the party ${people[i]}!`);
+        addPerson(`Welcome to the party ${people[i]}!`);
         guestList.push(people[i]);
     }
 }
-addElement();
